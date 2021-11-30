fdeb538dbae80d160ac97ae6764b1689ee5d8d36
lopesjr <atlopesjunior@gmail.com>
2021-11-29 21:28:12 -0300

    adjusts print readme & card

--- piloto/src/components/MyCard.vue
+++ piloto/src/components/MyCard.vue
@@ -1,36 +1,25 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
+  <v-card class="mx-auto" max-width="344">
     <v-img
       :src="require(`@/assets/${cardInfo.picture}`)"
       height="200px"
     ></v-img>
 
     <v-card-title>
-      {{cardInfo.name}}
+      {{ cardInfo.name }}
     </v-card-title>
 
     <v-card-subtitle>
-      {{cardInfo.country}}
+      {{ cardInfo.country }}
     </v-card-subtitle>
 
     <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-        More
-      </v-btn>
+      <v-btn color="orange lighten-2" text> More </v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
       </v-btn>
     </v-card-actions>
 
@@ -39,7 +28,7 @@
         <v-divider></v-divider>
 
         <v-card-text>
-          {{cardInfo.age}}, {{cardInfo.category}} pilot.
+          {{ cardInfo.age }}, {{ cardInfo.category }} pilot.
         </v-card-text>
       </div>
     </v-expand-transition>
@@ -47,11 +36,10 @@
 </template>
 
 <script>
-  export default {
-    props: ['cardid','cardInfo'],
-    data: () => ({
-      show: false,
-      picture: this.$store.state.pilots
-    }),
-  }
+export default {
+  props: ["cardid", "cardInfo"],
+  data: () => ({
+    show: false,
+  }),
+};
 </script>
